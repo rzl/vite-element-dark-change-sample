8c4922d3e3dbe99189d57d7904dad6d13c40040e
RZL <1120082028@qq.com>
2023-06-20 22:35:25 +0800

    Signed-off-by: RZL <1120082028@qq.com>

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './style.css'
+import App from './App.vue'
+var app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
